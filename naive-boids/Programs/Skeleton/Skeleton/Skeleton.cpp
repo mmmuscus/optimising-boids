b2b16db4e59ad0bdec06c976b3d9b3cd5c7e8460
mmmuscus <mmmuscus@gmail.com>
2021-07-30 01:24:18 +0200

    shader sketch

--- naive-boids/Programs/Skeleton/Skeleton/Skeleton.cpp
+++ naive-boids/Programs/Skeleton/Skeleton/Skeleton.cpp
@@ -37,6 +37,90 @@ const char* const fragmentSource = R"(
 	}
 )";
 
+class PhongShader : public GPUProgram
+{
+	const char* vertexSource = R"(
+		#version 330
+		precision highp float;
+
+		const vec3 wLightPos  = vec3(3, 4, 5);	// directional light source;
+		uniform mat4  MVP, M, Minv; // MVP, Model, Model-inverse
+		uniform vec3  wEye;         // pos of eye
+
+		layout(location = 0) in vec3  vtxPos;            // pos in modeling space
+		layout(location = 1) in vec3  vtxNorm;      	 // normal in modeling space
+		layout(location = 2) in vec2  vtxUV;
+
+		out vec3 wNormal;		    // normal in world space
+		out vec3 wView;             // view in world space
+		out vec3 wLight;		    // light dir in world space
+		out vec2 texcoord;
+
+		void main() {
+			gl_Position = vec4(vtxPos, 1) * MVP; // to NDC
+		    wView  = wEye - (vec4(vtxPos, 1) * M).xyz;
+			wLight = wLightPos;
+		    wNormal = (Minv * vec4(vtxNorm, 0)).xyz;
+		    texcoord = vtxUV;
+		}
+	)";
+
+	// fragment shader in GLSL
+	const char* fragmentSource = R"(
+		#version 330
+		precision highp float;
+
+		const vec3 ks = vec3(2, 2, 2);
+		const float shininess = 50.0f;
+		const vec3 La = vec3(0.1f, 0.1f, 0.1f);
+		const vec3 Le = vec3(2, 2, 2);    
+
+		uniform sampler2D diffuseTexture;
+
+		in  vec3 wNormal;       // interpolated world sp normal
+		in  vec3 wView;         // interpolated world sp view
+		in  vec3 wLight;        // interpolated world sp illum dir
+		in  vec2 texcoord;
+		
+        out vec4 fragmentColor; // output goes to frame buffer
+
+		void main() {
+			vec3 N = normalize(wNormal);
+			vec3 V = normalize(wView); 
+			if (dot(N, V) < 0) N = -N;
+			vec3 kd = texture(diffuseTexture, texcoord).rgb;
+			vec3 ka = kd * 3.14;
+			vec3 L = normalize(wLight);
+			vec3 H = normalize(L + V);
+			float cost = max(dot(N,L), 0), cosd = max(dot(N,H), 0);
+			fragmentColor = vec4(ka * La + (kd * cost + ks * pow(cosd, shininess)) * Le, 1);
+		}
+	)";
+
+public:
+
+	PhongShader() { create(vertexSource, fragmentSource, "fragmentColor"); }
+
+	void Bind(RenderState state) {
+		setUniform(state.M * state.V * state.P, "MVP");
+		setUniform(state.M, "M");
+		setUniform(state.Minv, "Minv");
+
+		setUniform(state.wEye, "wEye");
+
+		setUniform(state.kd, "kd");
+		setUniform(state.ks, "ks");
+		setUniform(state.ka, "ka");
+		setUniform(state.shininess, "shininess");
+
+		setUniform(state.La, "La");
+		setUniform(state.Le, "Le");
+		setUniform(state.wLightPos, "wLightPos");
+
+		// texture stuff
+	}
+};
+
 struct VertexData {
 	vec3 position, normal;
 
